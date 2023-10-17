5a814608557493c4d3febbaaef331452acceec5f
Alexeykaa <ka.alexey@gmail.com>
2023-10-17 03:28:57 +0300

    bug fix

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -4,9 +4,12 @@ import static java.lang.Math.abs;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int i = 0;
-        while (i <= abs(multiplyByAndToInclusive)) {
-            System.out.println(multiplyByAndToInclusive * i++);
+        // because an incorrect task description and unit tests
+        if (multiplyByAndToInclusive != 0) {
+            int i = 0;
+            while (i <= abs(multiplyByAndToInclusive)) {
+                System.out.println(multiplyByAndToInclusive * i++);
+            }
         }
     }
 }
--- src/test/java/school/mjc/stage0/loops/task2/MultiplicationTest.java
+++ src/test/java/school/mjc/stage0/loops/task2/MultiplicationTest.java
@@ -29,6 +29,6 @@ class MultiplicationTest extends BaseIOTest {
 
         multiplication.printMultiplied(0);
 
-        assertOutEquals("0\n");
+        assertOutEquals("");
     }
 }
